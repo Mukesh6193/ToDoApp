04a88af63cdcd56d43569df25f2ddae4b0c14f80
Mukesh Kumari <mukkumar28@publicisgroupe.net>
2024-01-26 21:36:42 +0530

    todo task manager functionalities added

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+package src.main.java;
+
+import src.main.java.controller.ToDoController;
+
+public class Main {
+    public static void main(String[] args) {
+        new ToDoController().runMyToDoApp();
+
+    }
+}
